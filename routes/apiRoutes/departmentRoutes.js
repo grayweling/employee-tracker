664e9b20cc92910c422b54fb0284525253b730a3
grayweling <jy6228@yahoo.com>
2022-05-15 08:18:50 -0500

    added routes

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
 
-
+router.get("/departments", (req, res) => {
+    db.query("SELECT * FROM department", (err, results) => {
+        if (err) {
+        res.status(500).send(err);
+        } else {
+        res.status(200).json(results.rows);
+        }
+    });
+})
 
 
 module.exports = router;
--- routes/apiRoutes/employeeRoutes.js
+++ routes/apiRoutes/employeeRoutes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../db/connection");
 
-
+router.get("/employees", (req, res) => {
+    db.query("SELECT * FROM employee", (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).json(results.rows);
+        }
+    })
+})
 
 
 module.exports = router;
